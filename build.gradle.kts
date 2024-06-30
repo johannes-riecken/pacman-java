ee0d1676dfbde767a69a7c46acfea97873a1178d
Johannes Riecken <johannes.riecken@gmail.com>
2024-06-30 14:41:29 +0200

    Switch build system to Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    application
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    implementation(libs.com.code.intelligence.jazzer.api)
+    implementation(libs.javax.json.javax.json.api)
+    implementation(libs.org.glassfish.javax.json)
+    implementation(libs.org.jetbrains.annotations)
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(22))
+    }
+}
+
+application {
+    mainClass.set("com.company.maze")
+}
